00db98581ece2226eed62a6cab86079c9f33e218
Ana -rr <102273751+anatoly-rap@users.noreply.github.com>
2023-07-27 22:59:10 -0600

    Update uploader.py
    clean up

--- uploader.py
+++ uploader.py
@@ -8,11 +8,10 @@ import shutil
 import yaml
 import argparse
 
+
 class AzureBlobUploader:
-    
     CONTAINER_NAME = ""
     blob_service_client = ""
-    
     def __init__(self, storage_account_key, storage_account_name, connection_string, local_path):
         self.blob_service_client = BlobServiceClient.from_connection_string(connection_string)
         self.CONTAINER_NAME = str(uuid.uuid1())
@@ -22,7 +21,6 @@ class AzureBlobUploader:
         except ResourceExistsError:
             print("Container already exists.")
         print("initializing uploader")
-        
     @classmethod
     def init_from_args(cls):
         parser = argparse.ArgumentParser()
@@ -33,14 +31,14 @@ class AzureBlobUploader:
         args = parser.parse_args()
         return cls(args.storage_account_key, args.storage_account_name, args.connection_string, args.local_path)
 
+    
     def run(self,all_file_names):
         with ThreadPool(processes=int(5)) as pool:
-            return pool.map(self.upload_image, all_file_names);
+            return pool.map(self.upload_image, all_file_names)
   
     def upload_all_images_in_folder(self):
-        
       all_file_names = [f for f in os.listdir(LOCAL_PATH)
-                    if os.path.isfile(os.path.join(LOCAL_PATH, f)) and ".jpg" in f]
+        if os.path.isfile(os.path.join(LOCAL_PATH, f)) and ".jpg" in f]
       self.run(all_file_names);
       
     def upload_image(self, file_name):
@@ -54,7 +52,6 @@ class AzureBlobUploader:
             blob_client.upload_blob(data, overwrite = True, content_settings = image_content_setting)
             
     def deleteLocDir(self):
-        
         for files in os.listdir(LOCAL_PATH):
             path = os.path.join(LOCAL_PATH, files)
         try:
